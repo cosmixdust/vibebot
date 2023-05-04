a7d17ad43305f3ed22c66aa67b22a952a81bdcac
Erial <111536069+cosmixdust@users.noreply.github.com>
2023-05-04 09:25:36 -0400

    new file for more phrases to be sent

---
+++ phrases.py
@@ -0,0 +1,22 @@
+monday = [
+    "It is Group-hug Monday today! I hope you're doing amazing and let's all have a great week!",
+    "Good morning folks, welcome to Group-hug Monday! As always, let's go for a great week!!",
+    "Group-hug Monday, everyone! Don't forget to drink some water :)",
+    "Monday! And you know what that means... Group-hug Monday! Come here everyone!"
+    ]
+    
+wednesday = [
+    "It is VVibe-check VVednesday! Are y'all vibing today? I am!",
+    "Welcome to VVibe-check VVednesday! Let me know how the vibes are today!",
+    "VVednesday of VVibe-check! Are the vibes okay? Are the vibes bad? Let us know!",
+    "",
+    ""
+    ]
+    
+thursday = [
+    "Thirsty Thursday! What are you drinking at this time? Let us know!"
+    "",
+    "",
+    "",
+    ""
+    ]

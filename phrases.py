09d4d4d2a71fa4514ad60439313de000862933fc
Erial <111536069+cosmixdust@users.noreply.github.com>
2023-05-07 09:36:12 -0400

    added more phrases

--- phrases.py
+++ phrases.py
@@ -9,14 +9,11 @@ wednesday = [
     "It is VVibe-check VVednesday! Are y'all vibing today? I am!",
     "Welcome to VVibe-check VVednesday! Let me know how the vibes are today!",
     "VVednesday of VVibe-check! Are the vibes okay? Are the vibes bad? Let us know!",
-    "",
-    ""
+    "Are we vibing? Hopefully, because today is VVibe-check VVedneday!!",
     ]
     
 thursday = [
     "Thirsty Thursday! What are you drinking at this time? Let us know!"
-    "",
-    "",
-    "",
-    ""
+    "I would love a glass of water... Because it's Thirsty Thursday! What are you drinking today?",
+    "Thirsty Thursday! Drinkin' time! Thirsty Thursday! Drinkin' time! Thirsty Thursday! Drinkin' time!",
     ]
